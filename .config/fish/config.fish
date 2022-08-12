939f6ba5f5d72284948be6af3a8743e4a5927a64
Bernt Anker <bernt.anker@gmail.com>
2022-08-12 10:19:25 +0200

    removed old bare config from fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,3 @@ if status is-interactive
 end
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
