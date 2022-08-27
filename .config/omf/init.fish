112dbec396982c84e7bc1413ebaa40f6350412b2
Bernt Anker <bernt.anker@gmail.com>
2022-08-27 12:15:05 +0200

    Updating fish

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,7 +1,6 @@
 set OS (uname -s)
 if test $OS = "Linux"
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    fenv source /home/bernt/.nix-profile/etc/profile.d/nix.sh 
+  fenv source /home/bernt/.nix-profile/etc/profile.d/nix.sh 
 else if $OS = "Darwin" 
   eval "$(/opt/hombrew/bin/brew shellenv)"
 end
