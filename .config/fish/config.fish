c1159fcf4a1db12bf203158f7e377920faa32ca9
Bernt Anker <bernt.anker@gmail.com>
2022-08-27 11:53:40 +0200

    Updating fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,3 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set OS (uname -s)
-if test $OS = "Linux"
-    sh /home/bernt/.nix-profile/etc/profile.d/nix.sh | source 
-else if $OS = "Darwin" 
-  eval "$(/opt/hombrew/bin/brew shellenv)"
-end
-
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-
-
