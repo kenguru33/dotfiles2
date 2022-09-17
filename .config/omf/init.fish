6180fdb5f67be4ea75d6740f3b9465d565ea079f
Bernt Anker <bernt.anker@gmail.com>
2022-09-18 00:09:28 +0200

    fixed init fish

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,8 +1,8 @@
 set OS (uname -s)
 if test $OS = "Linux"
   fenv source /home/bernt/.nix-profile/etc/profile.d/nix.sh 
-else if $OS = "Darwin" 
-  eval "$(/opt/hombrew/bin/brew shellenv)"
+else if test $OS = "Darwin" 
+  eval $(/opt/homebrew/bin/brew shellenv)
 end
 
 set -gx VOLTA_HOME "$HOME/.volta"
